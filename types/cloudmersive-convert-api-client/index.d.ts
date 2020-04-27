7f1e9b61253cd1daec70432475438c0effb28903
Jason Luboff <jluboff@gmail.com>
2020-04-27 15:10:49 -0700

    Completed ConvertDataApi typings

--- types/cloudmersive-convert-api-client/index.d.ts
+++ types/cloudmersive-convert-api-client/index.d.ts
@@ -109,12 +109,12 @@ export interface ApiClient {
 }
 
 export interface CompareDocumentDocx {
-    (inputFile1: Buffer, inputFile2: Buffer, callback: (error: any, data: string, response: any) => any): any
+    (inputFile1: Buffer, inputFile2: Buffer, callback: (error: any, data: string, response: any) => any): any;
 }
 export interface CompareDocumentApi {
     /**
      * Compare two Office Word Documents (docx) files and highlight the differences
-     * @param inputFile1 
+     * @param inputFile1
      * buffer
      * @param inputFile2
      * buffer
@@ -131,14 +131,138 @@ export class CompareDocumentApi implements CompareDocumentApi {
 
 interface ConvertDataCsvToJsonOptions {
     /**
-     * Boolean | Optional; If true, the first row will be used as the labels for the columns; 
-     * if false, columns will be named Column0, Column1, etc.  
+     * Boolean | Optional; If true, the first row will be used as the labels for the columns;
+     * if false, columns will be named Column0, Column1, etc.
      * Default is true.  Set to false if you are not using column headings, or have an irregular column structure.
      */
-columnNamesFromFirstRow: boolean
+    columnNamesFromFirstRow: boolean;
 }
+
 export interface ConvertDataCsvToJson {
-    (inputFile: Buffer, opts: ConvertDataCsvToJsonOptions, callback:(error: any, data: object, response: any) => any) : any
+    (
+        inputFile: Buffer,
+        opts: ConvertDataCsvToJsonOptions | {},
+        callback: (error: any, data: object[], response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataToJson {
+    (inputFile: Buffer, callback: (error: any, data: object[], response: any) => any): any;
+}
+
+export interface ConvertDataXmlFilterWithXPath {
+    (
+        xPathExpression: string,
+        inputFile: Buffer,
+        callback: (
+            error: any,
+            data: { Successful: boolean; XmlNodes: string[]; ResultCount: number },
+            response: any,
+        ) => any,
+    ): any;
+}
+
+export interface XmlQueryWithXQueryResult {
+    Successful: boolean;
+    ResultingXml: string;
+    ErrorMessage: string;
+}
+
+export interface ConvertDataXmlQueryWithXQuery {
+    (
+        inputFile: Buffer,
+        xQuery: string,
+        callback: (error: any, data: XmlQueryWithXQueryResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlQueryWithXQueryMulti {
+    (
+        inputFile: Buffer,
+        xQuery: string,
+        opts: {
+            inputFile2: Buffer;
+            inputFile3: Buffer;
+            inputFile4: Buffer;
+            inputFile5: Buffer;
+            inputFile6: Buffer;
+            inputFile7: Buffer;
+            inputFile8: Buffer;
+            inputFile9: Buffer;
+            inputFile10: Buffer;
+        },
+        callback: (error: any, data: XmlQueryWithXQueryResult, response: any) => any,
+    ): any;
+}
+
+export interface XmlSetValuesResult {
+    Successful: boolean;
+    ResultingXmlDocument: string;
+    NodesEditedCount: number;
+}
+
+export interface ConvertDataXmlEditSetValueWithXPath {
+    (
+        inputFile: Buffer,
+        xPathExpression: string,
+        xmlValue: string,
+        callback: (error: any, data: XmlSetValuesResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlEditReplaceWithXPath {
+    (
+        inputFile: Buffer,
+        xPathExpression: string,
+        xmlNodeReplacement: string,
+        callback: (error: any, data: XmlSetValuesResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlEditAddChildWithXPath {
+    (
+        inputFile: Buffer,
+        xPathExpression: string,
+        xmlNodeToAdd: string,
+        callback: (error: any, data: XmlSetValuesResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlEditAddAttributeWithXPath {
+    (
+        inputFile: Buffer,
+        xPathExpression: string,
+        xmlAttributeName: string,
+        xmlAttributeValue: string,
+        callback: (error: any, data: XmlSetValuesResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlRemoveWithXPath {
+    (
+        xPathExpression: string,
+        inputFile: Buffer,
+        callback: (
+            error: any,
+            data: Omit<XmlSetValuesResult, 'NodesEditedCount'> & {
+                XmlNodesRemoved: string[];
+                NodesRemovedCount: number;
+            },
+            response: any,
+        ) => any,
+    ): any;
+}
+
+export interface ConvertDataXmlEditRemoveAllChildNodesWithXPath {
+    (
+        inputFile: Buffer,
+        xPathExpression: string,
+        callback: (error: any, data: XmlSetValuesResult, response: any) => any,
+    ): any;
+}
+
+export interface ConvertDataJsonToXml {
+    (jsonObject: object, callback: (error: any, data: string, response: any) => any): any;
 }
 
 export interface ConvertDataApi {
@@ -152,13 +276,194 @@ export interface ConvertDataApi {
      * boolean | Optional; If true, the first row will be used as the labels for the columns;
      * if false, columns will be named Column0, Column1, etc.
      * Default is true.  Set to false if you are not using column headings, or have an irregular column structure.
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
      */
     convertDataCsvToJson: ConvertDataCsvToJson;
+    /**
+     * Convert an Excel XLSX file to a JSON object array
+     * @param inputFile
+     * buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXlsxToJson: ConvertDataToJson;
+    /**
+     * Convert an Excel (97-2003) XLS file to a JSON object array
+     * @param inputFile
+     * buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXlsToJson: ConvertDataToJson;
+    /**
+     * Convert an XML string or file into JSON
+     * @param inputFile
+     * buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlToJson: ConvertDataToJson;
+    /**
+     * Return the reuslts of filtering, selecting an XML document with an XPath expression
+     * @param xPathExpression
+     * string
+     * @param inputFile
+     * Buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlFilterWithXPath: ConvertDataXmlFilterWithXPath;
+    /**
+     * Return the reuslts of querying a single XML document with an XQuery expression. Supports XQuery 3.1 and earlier. This API is optimized for a single XML document as input.
+     * Provided XML document is automatically loaded as the default context;
+     * to access elements in the document, simply refer to them without a document reference, such as bookstore/book.
+     * @param inputFile
+     * buffer
+     * @param xQuery
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlQueryWithXQuery: ConvertDataXmlQueryWithXQuery;
+    /**
+     * Return the reuslts of querying an XML document with an XQuery expression. Supports XQuery 3.1 and earlier.
+     * This API is optimized for multiple XML documents as input. You can refer to the contents of a given document by name, for example doc("books.xml") or doc("restaurants.xml")
+     * if you included two input files named books.xml and restaurants.xml. If input files contain no file name, they will default to file names input1.xml, input2.xml and so on.
+     * @param inputFile
+     * buffer
+     * @param xQuery
+     * string
+     * @param opts
+     * object
+     * @param opts.inputFileX
+     * Buffer - Where X is a number between 2-10
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlQueryWithXQueryMulti: ConvertDataXmlQueryWithXQueryMulti;
+    /**
+     * Return the reuslts of editing an XML document by setting the contents of all of the nodes that match an input XPath expression. Supports elements and attributes.
+     * @param inputFile
+     * buffer
+     * @param xPathExpression
+     * string
+     * @param xmlValue
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlEditSetValueWithXPath: ConvertDataXmlEditSetValueWithXPath;
+    /**
+     * Return the reuslts of editing an XML document by replacing all of the nodes that match an input XPath expression with a new XML node expression.
+     * @param inputFile
+     * buffer
+     * @param xPathExpression
+     * string
+     * @param xmlNodeReplacement
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlEditReplaceWithXPath: ConvertDataXmlEditReplaceWithXPath;
+    /**
+     * Return the reuslts of editing an XML document by adding an XML node as a child to all of the nodes that match an input XPath expression.
+     * @param inputFile
+     * buffer
+     * @param xPathExpression
+     * string
+     * @param xmlNodeToAdd
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlEditAddChildWithXPath: ConvertDataXmlEditAddChildWithXPath;
+    /**
+     * Return the reuslts of editing an XML document by adding an attribute to all of the nodes that match an input XPath expression.
+     * @param inputFile
+     * buffer
+     * @param xPathExpression
+     * string
+     * @param xmlAttributeName
+     * string
+     * @param xmlAttributeValue
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlEditAddAttributeWithXPath: ConvertDataXmlEditAddAttributeWithXPath;
+    /**
+     * Return the reuslts of editing an XML document by removing all of the nodes that match an input XPath expression
+     * @param xPathExpression
+     * string
+     * @param inputFile
+     * buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlRemoveWithXPath: ConvertDataXmlRemoveWithXPath;
+    /**
+     * Return the reuslts of editing an XML document by removing all child nodes of the nodes that match an input XPath expression.
+     * @param inputFile
+     * buffer
+     * @param xPathExpression
+     * string
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataXmlEditRemoveAllChildNodesWithXPath: ConvertDataXmlEditRemoveAllChildNodesWithXPath;
+    /**
+     * Convert an XML string or file into JSON
+     * @param inputFile
+     * buffer
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     *
+     * NOTE: In Cloudmersive's documentation, this method is mentioned twice for two different purposes with
+     * different return values
+     */
+    // convertDataXmlToJson: ConvertDataXmlToJson;
+    /**
+     * Convert a JSON object into XML
+     * @param jsonObject
+     * object
+     * @param callback
+     * function - The callback function, accepting three arguments:
+     * error, data, response
+     */
+    convertDataJsonToXml: ConvertDataJsonToXml;
 }
 
 export class ConvertDataApi implements ConvertDataApi {
     constructor(apiClient?: ApiClient);
-    convertDataCsvToJson: ConvertDataCsvToJson
+    convertDataCsvToJson: ConvertDataCsvToJson;
+    convertDataXlsxToJson: ConvertDataToJson;
+    convertDataXlsToJson: ConvertDataToJson;
+    convertDataXmlToJson: ConvertDataToJson;
+    convertDataXmlFilterWithXPath: ConvertDataXmlFilterWithXPath;
+    convertDataXmlQueryWithXQuery: ConvertDataXmlQueryWithXQuery;
+    convertDataXmlQueryWithXQueryMulti: ConvertDataXmlQueryWithXQueryMulti;
+    convertDataXmlEditSetValueWithXPath: ConvertDataXmlEditSetValueWithXPath;
+    convertDataXmlEditReplaceWithXPath: ConvertDataXmlEditReplaceWithXPath;
+    convertDataXmlEditAddChildWithXPath: ConvertDataXmlEditAddChildWithXPath;
+    convertDataXmlEditAddAttributeWithXPath: ConvertDataXmlEditAddAttributeWithXPath;
+    convertDataXmlRemoveWithXPath: ConvertDataXmlRemoveWithXPath;
+    convertDataXmlEditRemoveAllChildNodesWithXPath: ConvertDataXmlEditRemoveAllChildNodesWithXPath;
+    convertDataJsonToXml: ConvertDataJsonToXml;
 }
 
 export const ApiClient: ApiClient;
